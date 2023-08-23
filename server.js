8ddeb8e1f84f81ad29eac9b095ae910b97b5c5e3
Vidyamg <110501076+Vidyamgunashekar@users.noreply.github.com>
2023-08-23 14:04:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Vidya on Aug 21st');
+  const currentDate = new Date();
+  const formattedDate = currentDate.toDateString();
+  res.send(`Hello there. Today is ${formattedDate}.`);
 });
 
-app.get('/api/health-check',(req,res) => {
-res.status(200).send('some text');
-
+app.get('/api/health-check', (req, res) => {
+  res.status(200).send('Instances are healthy');
 });
 
 app.listen(PORT, HOST, () => {
