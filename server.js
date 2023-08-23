a2b27e1d06db7dbcb09161f52222a0541b3dce98
vidyamgunashekar <apple@apples-MBP.home>
2023-08-23 10:05:10 -0700

    buildspec chnged to reflect us-west-1

--- server.js
+++ server.js
@@ -3,13 +3,18 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 4500;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Vidya on Aug 21st');
+});
+
+app.get('/api/health-check',(req,res) => {
+res.status(200).send('some text');
+
 });
 
 app.listen(PORT, HOST, () => {
